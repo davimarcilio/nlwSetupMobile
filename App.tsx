dcb5b37a91d4e12657d0cf840b7c603122f5bc39
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-16 22:06:36 -0300

    Status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -24,7 +23,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
     </View>
   );
 }
@@ -32,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#09090a",
     alignItems: "center",
     justifyContent: "center",
   },
