89c49d2cb21649b5d430031e047fa715b75b1264
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-17 21:16:38 -0300

    installing tailwind on expo

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: "#09090a",
+    },
+    fontFamily: {
+      regular: "Inter_400Regular",
+      semibold: "Inter_600Semibold",
+      bold: "Inter_600Bold",
+      extrabold: "Inter_600ExtraBold",
+    },
+  },
+  plugins: [],
+};
