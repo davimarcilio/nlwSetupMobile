3c4b55dad7e6b11552f6e03070b4a492aa6dac52
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-18 21:14:07 -0300

    New created

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
-
+import { Feather } from "@expo/vector-icons";
+import colors from "tailwindcss/colors";
 const avaliableWeekDays = [
   "Domingo",
   "Segunda-Feira",
@@ -28,7 +35,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 50 }}
+        showsVerticalScrollIndicator={false}
+      >
         <BackButton />
         <Text className="mt-6 text-white font-extrabold text-3xl">
           Criar hábito
@@ -38,7 +48,11 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <TextInput
+          placeholder="Exercícios, dormir bem, etc..."
+          className="h-12  pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          placeholderTextColor={colors.zinc[400]}
+        />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
@@ -52,6 +66,16 @@ export function New() {
             title={weekDay}
           />
         ))}
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
