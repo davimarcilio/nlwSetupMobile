605d6c6dc1d759943cc97690edd9b6d1faa2de41
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-18 21:32:25 -0300

    fixes

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -50,7 +50,7 @@ export function New() {
 
         <TextInput
           placeholder="Exercícios, dormir bem, etc..."
-          className="h-12  pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+          className="h-12  pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
           placeholderTextColor={colors.zinc[400]}
         />
 
