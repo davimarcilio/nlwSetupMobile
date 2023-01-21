8e154fe3f73e4dd3871fa2d2f12454ea5610584c
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-20 21:14:10 -0300

    Animated progressbar/checkbox

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,15 +1,30 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface ProgressBarProps {
   progress?: number;
 }
 export function ProgressBar({ progress = 0 }: ProgressBarProps) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress);
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4 ">
-      <View
-        style={{ width: `${progress}%` }}
-        className="h-3 rounded-xl bg-violet-600"
-      />
+      <Animated.View style={style} className="h-3 rounded-xl bg-violet-600" />
     </View>
   );
 }
